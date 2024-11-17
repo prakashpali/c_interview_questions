55bf36d4bbe094b3d792c0e5cab092d1b128f2c6
Prakash Pali <prakashpali@Prakashs-MacBook-Air.local>
2024-11-17 19:31:45 +0530

    Adding more cases and complexities to structures/bit_fields_in_c.c

--- structures/bit_fields_in_c.c
+++ structures/bit_fields_in_c.c
@@ -4,14 +4,15 @@
 
 #include "../utils.h"
 
-typedef struct __attribute__((packed))
+typedef struct
 {
-    unsigned char a : 8;
-    unsigned char b : 8;
-    unsigned char c : 4;
-    unsigned char d : 4;
+    unsigned char a : 8;    // This is same as unsigned char a : 8;
+    unsigned char b : 8;    // Bit field size take precedence over data type
+    unsigned char c : 3;    // This nibble is combined with next nibble in actual memory
+    unsigned char d : 3;    // Bit fields are always packed. i.e. 1st 8, 8 bits and last 3,3,1,1 bits are combined to take up 3 bytes (24 bits).
     unsigned char e : 1;
     unsigned char f : 1;
+    unsigned int x;         // This is another structure field. Above bit fields take up 3 bytes, this int takes up 4 bytes. But both are aligned to take up 8 bytes.
 } test;
 
 int main()
@@ -32,21 +33,23 @@ int main()
     t.c = 0x56;
     printf("a = 0x%X, b = 0x%X, c = 0x%X, d = 0x%X, e = 0x%X, f = 0x%X\n", t.a, t.b, t.c, t.d, t.e, t.f);
 
-    t.d = 0x78;
+    t.d = 0x79;
     printf("a = 0x%X, b = 0x%X, c = 0x%X, d = 0x%X, e = 0x%X, f = 0x%X\n", t.a, t.b, t.c, t.d, t.e, t.f);
 
-    t.e = 0;
+    t.e = 2;
     printf("a = 0x%X, b = 0x%X, c = 0x%X, d = 0x%X, e = 0x%X, f = 0x%X\n", t.a, t.b, t.c, t.d, t.e, t.f);
 
     t.f = 1;
     printf("a = 0x%X, b = 0x%X, c = 0x%X, d = 0x%X, e = 0x%X, f = 0x%X\n", t.a, t.b, t.c, t.d, t.e, t.f);
+
+    t.x = 0x12345678;
 
     printf("Address of t = 0x%lX\n", (unsigned long)&t);
 
     /* Address of bit field is not allowed in c. */
     // printf("Address of t.a = 0x%lX", (unsigned long)&(t.a));
 
-    printf("32 bit value at address t = 0x%08X\n", *((unsigned int *)&t));
+    printf("32 bit value at address t = 0x%lX\n", *((unsigned long *)&t));
     printf("=====================================================\n");
 
     return 0;

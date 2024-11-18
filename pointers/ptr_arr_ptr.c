521cd06a0dc2d4c452d99a11140365f2e70bb3eb
Prakash Pali <prakashpali@Prakashs-MacBook-Air.local>
2024-11-18 23:44:03 +0530

    Adding more cases to pointers/ptr_arr_ptr.c

--- pointers/ptr_arr_ptr.c
+++ pointers/ptr_arr_ptr.c
@@ -11,4 +11,31 @@ int main(void)
     printf("%d\n", *ptr++); // 22
     printf("%d\n", *++ptr); // 44
     printf("%d\n", *--ptr); // 33
+
+
+    /* This is not a valid assignment. ptr1 is a double pointer. &arr is pointer to an array of 5 elements. */
+    // int **ptr1 = &arr;
+
+    /* This is a valid assignment. *ptr1 is a pointer to array of 5 elements. */
+    int (*ptr1)[5] = &arr;
+    printf("sizeof(arr) = %lu\n", sizeof(arr));
+    printf("sizeof(ptr1) = %lu\n", sizeof(ptr1));
+    printf("sizeof(*ptr1) = %lu\n", sizeof(*ptr1));
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("(*ptr1)[i] = %d\n", (*ptr1)[i]);
+    }
+
+    int temp = 10;
+    int (*ptr2)[temp] = &arr;
+    printf("sizeof(arr) = %lu\n", sizeof(arr));
+    printf("sizeof(ptr2) = %lu\n", sizeof(ptr2));
+    printf("sizeof(*ptr2) = %lu\n", sizeof(*ptr2));
+
+    for(int i = 0; i < temp; i++)
+    {
+        printf("(*ptr2)[i] = %d\n", (*ptr2)[i]);
+    }
+
 }

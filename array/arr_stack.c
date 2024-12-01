f77efca993a14bccc1fbc29f9aee6974d3f5bc70
Prakash Pali <prakashpali@Prakashs-MacBook-Air.local>
2024-12-01 23:09:14 +0530

    Adding array/arr_stack.c

--- array/arr_stack.c
+++ array/arr_stack.c
@@ -2,20 +2,47 @@
  * Stack using array.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "../utils.h"
 
+#define STACK_SIZE  (20)
+
+int stack[] = {0,1,2,3,4,5,6,7,8,9};
+int stack_top = -1;
+
+static void push(int data)
+{
+    if(stack_top > STACK_SIZE)
+    {
+        printf("stack overflow\n");
+        return;
+    }
+
+    stack[++stack_top] = data;
+
+}
+
+static int pop(void)
+{
+    if(stack_top < 0)
+    {
+        printf("stack underflow\n");
+        return -1;
+    }
+
+    return stack[stack_top--];
+}
 
 int main()
 {
-    int A[] = {0,1,2,3,4,5,6,7,8,9};
-
-    int len_arr = (sizeof(A)/sizeof(A[0]));
-
-    int *arr = &A[4];
+    push(2);
+    push(4);
+    push(5);
+    push(6);
+    push(7);
+    push(8);
 
     printf("=====================================================\n");
-    printf("A[-1] = 0x%X\n", arr[-1]);
+    printf("Popped data = %d\n", pop());
     printf("=====================================================\n");
 
     return 0;

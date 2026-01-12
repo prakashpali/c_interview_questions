f852cde94e6337b4e78c6be38cf76346c8bc1db7
prakashpali <prakash14pali@gmail.com>
2026-01-12 22:53:20 +0530

    Modified palindrome solution

--- numbers/q18_num_find_palindrome.c
+++ numbers/q18_num_find_palindrome.c
@@ -1,24 +1,33 @@
 
 #include "../utils.h"
 
+static int reverse_num(int num)
+{
+    int rev = 0;
+
+    while(num > 0)
+    {
+        int digit = num % 10;
+        rev = rev * 10 + digit;
+        num /= 10;
+    }
+
+    return rev;
+}
+
 static bool is_palindrome(int num)
 {
     int reversed = 0;
     int original = num;
 
-    while (num > 0)
-    {
-        int digit = num % 10;
-        reversed = reversed * 10 + digit;
-        num /= 10;
-    }
+    reversed = reverse_num(num);
 
     return original == reversed;
 }
 
 int main()
 {
-    int number = 123212;
+    int number = 12321;
 
     bool result = is_palindrome(number);
 

b3dd3a54006b9f85bb8e4188659884aa9399294b
prakashpali <prakash14pali@gmail.com>
2026-01-19 22:41:31 +0530

    Minor modifications

--- pointers/ptr_str.c
+++ pointers/ptr_str.c
@@ -6,8 +6,12 @@ int main()
     char *str = "His";
     int i;
 
+    printf("String length: %lu\n", strlen(str));
+
+    /* This loop keeps incrememting str, strlen(str) will keep reducing. This loop will exit early. */
     for (i = 0; i < strlen(str); i++)
     {
+        printf("Value of i: %d\n", i);
         printf("%s\n", str++);
         printf("%lu\n", strlen(str));
     }

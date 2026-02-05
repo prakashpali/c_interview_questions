3cbd33e4506d6708c3ed949bd35def4f6c2ef984
prakashpali <prakash14pali@gmail.com>
2026-02-05 21:37:49 +0530

    Added more python questions

---
+++ python/variables/00_variable_scope.py
@@ -0,0 +1,12 @@
+# This function modifies global variable 's'
+def f():
+    global s
+    print(s)
+    s = "Look for Geeksforgeeks Python Section"
+    print(s)
+
+
+# Global Scope
+s = "Python is great !"
+f()
+print(s)

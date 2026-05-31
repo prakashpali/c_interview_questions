28300205b57231231f12dfac7977f0aaa14cd6b2
prakashpali <prakash14pali@gmail.com>
2026-05-31 13:29:47 +0530

    Add alignment macro utility

---
+++ numbers/q22_num_alignment.c
@@ -0,0 +1,30 @@
+
+
+#include "../utils.h"
+
+#define ALIGNMENT_8B           (8U)
+#define ALIGNMENT_32B          (32U)
+
+/**
+ * 1 --> 8
+ * 2 --> 8
+ * 3 --> 8
+ * .
+ * .
+ * 7 --> 8
+ * 8 --> 8
+ * 9 --> 16
+ */
+#define ALIGN_TO_8B(num)    ((num) + (ALIGNMENT_8B - 1) & ~(ALIGNMENT_8B - 1))
+#define ALIGN_TO_32B(num)   ((num) + (ALIGNMENT_32B - 1) & ~(ALIGNMENT_32B - 1))
+
+int main (void)
+{
+    int num = 15;
+    printf("==========================\n");
+    printf("num = %d, aligned num = %d\n", num, ALIGN_TO_8B(num));
+    printf("num = %d, aligned num = %d\n", num, ALIGN_TO_32B(num));
+    printf("==========================\n");
+
+    return 0;
+}
